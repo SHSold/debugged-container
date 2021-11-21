a8eb4fd9ffe619619894aa6a372fc53f694f3396
Stefan Sold <ssold@web.de>
2021-11-21 12:05:19 +0100

    Initial commit of fixed docker container

---
+++ server.js
@@ -0,0 +1,28 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getData() {
+	return "This is the Date:"
+}
+
+function getDate() {
+  return new Date();
+}
+
+async function main() {
+  while(true) {
+    try  {
+		console.log(getData());
+		console.log(getDate());
+    }
+    catch(e) {
+      console.log(e);
+    }
+    finally {
+      await sleep(5000);
+    }
+  }
+}
+
+main();
